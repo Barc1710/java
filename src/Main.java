eb6a2ccfa96a42fad73e4b4077993b1869e76d39
Belther Rodas <ba.rodascu@unsm.edu.pe>
2026-08-08 12:40:47 -0500

    feat: implementando novo metodo

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ void main() {
     mt.edad = 34;
     mt.mayor();
     rebase rb = new rebase();
+    rb.nombre="pedro";
+    rb.entrar();
     rb.menor();
+    otrometodo om = new otrometodo();
+    om.registrar();
 }
--- src/rebase.java
+++ src/rebase.java
@@ -1,8 +1,14 @@
 public class rebase {
-    String nombre = "pedro";
+    String nombre;
     int edad = 23;
 
     void menor(){
         System.out.println("hola");
+    }
+
+    void entrar(){
+        if (nombre.equals("pedro")){
+            System.out.println("hola "+ nombre);
+        }
     }
 }
