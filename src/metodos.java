ab59c6c366f54984e5610d0893dc09d9d7b4984f
Belther Rodas <ba.rodascu@unsm.edu.pe>
2026-08-08 12:10:17 -0500

    feat: add new methods and update main

---
+++ src/metodos.java
@@ -0,0 +1,12 @@
+public class metodos {
+    String nombre = "pepe";
+    int edad = 23;
+
+    void mayor(){
+        if (edad>=18){
+            System.out.println("Es mayor de edad");
+        }else {
+            System.out.println("es menor");
+        }
+    }
+}
